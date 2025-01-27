5228e57ec128f2f04fa9b76570eb7318d896fd90
SquadranHelix <thezackerr8@gmail.com>
2025-01-28 00:31:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function toggleMenu() {
+    const sidebar = document.getElementById('sidebar');
+    const content = document.getElementById('main-content');
+    sidebar.classList.toggle('open');
+    if (sidebar.classList.contains('open')) {
+        content.style.marginLeft = '250px';
+    } else {
+        content.style.marginLeft = '0';
+    }
+}
 
